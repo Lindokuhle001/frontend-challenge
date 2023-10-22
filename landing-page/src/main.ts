ea6d129b0a09d3255688421e2409f0302afb98aa
Lindokuhle Shabalala <68709767+Lindokuhle001@users.noreply.github.com>
2023-10-22 12:02:19 +0000

    codespace

--- landing-page/src/main.ts
+++ landing-page/src/main.ts
@@ -1,1 +1,40 @@
-import './style.css'
+const closeMenu = document.getElementById("close-menu") as HTMLButtonElement | null;
+const openMenu = document.getElementById("open-menu") as HTMLButtonElement | null;
+const nav = document.getElementById("nav") as HTMLElement | null;
+const overlay = document.getElementById("overlay") as HTMLElement | null;
+
+if (openMenu && closeMenu && nav && overlay) {
+  openMenu.addEventListener("click", (_e: MouseEvent) => {
+    openMenu.classList.add("inactive");
+    closeMenu.classList.add("active");
+
+    closeMenu.classList.remove("inactive");
+    openMenu.classList.remove("active");
+
+    nav.classList.toggle("active");
+
+    overlay.classList.toggle("overlay-active");
+  });
+
+  closeMenu.addEventListener("click", (_e: MouseEvent) => {
+    openMenu.classList.add("active");
+    closeMenu.classList.add("inactive");
+
+    closeMenu.classList.remove("active");
+    openMenu.classList.remove("inactive");
+
+    nav.classList.toggle("active");
+    overlay.classList.toggle("overlay-active");
+  });
+
+  overlay.addEventListener("click", (_e: MouseEvent) => {
+    openMenu.classList.add("active");
+    closeMenu.classList.add("inactive");
+
+    closeMenu.classList.remove("active");
+    openMenu.classList.remove("inactive");
+
+    nav.classList.toggle("active");
+    overlay.classList.toggle("overlay-active");
+  });
+}
